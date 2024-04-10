95d0cadca0251f29683d34d4351deceb94b06422
checkerowl <166591428+checkerowl@users.noreply.github.com>
2024-04-10 14:10:50 +0200

    Update vite.config.ts reportOnFailure

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     environment: "jsdom",
     coverage: {
       reporter: ["text", "json", "json-summary"],
+      reportOnFailure: true,
     },
     setupFiles: ["./test/setup.ts"],
   },
